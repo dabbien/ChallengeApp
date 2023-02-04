f3b466116b5867971b3f9f9fc00a2f58efe81bc3
dabbien <88117161+dabbien@users.noreply.github.com>
2023-02-05 00:10:38 +0100

    day 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,60 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string name = "";
+char sex = 'm';
+int age = 17;
+
+if (sex.ToString().Length != 0)
+    if (age < 18)
+    {
+        if (sex == 'F' || sex == 'f')
+        {
+            Console.WriteLine("The person is an underaged female.");
+        }
+        else if (sex == 'M' || sex == 'm')
+        {
+            Console.WriteLine("The person is an underaged male.");
+        }
+        else
+        {
+            Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
+        }
+    }
+    else
+    {
+        if (name.Length == 0)
+        {
+            if (sex == 'F' || sex == 'f')
+            {
+                if (age < 30)
+                {
+                    Console.WriteLine("The person is a female under 30 years old.");
+                }
+                else
+                {
+                    Console.WriteLine("The person is a female over 30 years old.");
+                }
+            }
+            else if (sex == 'M' || sex == 'm')
+            {
+                if (age < 30)
+                {
+                    Console.WriteLine("The person is a male under 30 years old.");
+                }
+                else
+                {
+                    Console.WriteLine("The person is a male over 30 years old.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
+            }
+        }
+        else
+        {
+            Console.WriteLine(name + ", " + age + " years old.");
+        }
+    }
+else
+{
+    Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
+}
