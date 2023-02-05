f93afea712ab4b5589b8f6c4dbe873d2d56e03fa
dabbien <88117161+dabbien@users.noreply.github.com>
2023-02-06 00:23:27 +0100

    day 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,60 +1,15 @@
-﻿string name = "";
-char sex = 'm';
-int age = 17;
+﻿int number = 4566;
+string numberInString = number.ToString();
+char[] numbers = numberInString.ToArray();
+int[] result = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-if (sex.ToString().Length != 0)
-    if (age < 18)
-    {
-        if (sex == 'F' || sex == 'f')
-        {
-            Console.WriteLine("The person is an underaged female.");
-        }
-        else if (sex == 'M' || sex == 'm')
-        {
-            Console.WriteLine("The person is an underaged male.");
-        }
-        else
-        {
-            Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
-        }
-    }
-    else
-    {
-        if (name.Length == 0)
-        {
-            if (sex == 'F' || sex == 'f')
-            {
-                if (age < 30)
-                {
-                    Console.WriteLine("The person is a female under 30 years old.");
-                }
-                else
-                {
-                    Console.WriteLine("The person is a female over 30 years old.");
-                }
-            }
-            else if (sex == 'M' || sex == 'm')
-            {
-                if (age < 30)
-                {
-                    Console.WriteLine("The person is a male under 30 years old.");
-                }
-                else
-                {
-                    Console.WriteLine("The person is a male over 30 years old.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
-            }
-        }
-        else
-        {
-            Console.WriteLine(name + ", " + age + " years old.");
-        }
-    }
-else
+foreach (char digit in numbers)
 {
-    Console.WriteLine("Please select persons sex in a single letter format: F - for a female, M - for a male.");
+    int index = digit - '0';
+    result[index]++;
+}
+
+for (int i = 0; i < result.Length; i++)
+{
+    Console.WriteLine($"{i} => {result[i]}");
 }
